442443ee8a7fb19128fd3c56b44128ac738b9a81
luisfelipedussan <lf.dussan@uniandes.edu.co>
2024-05-12 19:33:43 -0500

    Avances profile

--- RestaurantsApp/RestaurantsApp/ViewModel/ProfileViewModel.swift
+++ RestaurantsApp/RestaurantsApp/ViewModel/ProfileViewModel.swift
@@ -6,14 +6,67 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseStorage
 import FirebaseFirestore
 import FirebaseAuth
+import SwiftUI
 
 // Función para obtener la imagen del usuario
 
 class ProfileViewModel:ObservableObject {
     @Published var documents: [DocumentSnapshot] = []
     @Published var profileName: String = ""
+    @Published var profileImage: Image?
+    
+
+
+    func saveProfileImage(image: UIImage) {
+        guard let data = image.jpegData(compressionQuality: 0.8) else {
+            // Manejar el caso en el que no se pueda convertir la imagen a datos JPEG
+            return
+        }
+        
+        guard let currentUserID = Auth.auth().currentUser?.uid else {
+            // Manejar el caso en el que el usuario actual no esté autenticado
+            return
+        }
+        
+        // Ruta completa del archivo en Firebase Storage
+        let filename = "\(currentUserID).jpg"
+        
+        // Referencia al archivo existente en Firebase Storage
+        let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+        
+        // Eliminar el archivo existente (si lo hay)
+        storageRef.delete { error in
+            if let error = error {
+                // Manejar el caso en el que ocurra un error al eliminar el archivo existente
+                print("Error al eliminar el archivo existente:", error.localizedDescription)
+            }
+            
+            // Subir la nueva imagen a Firebase Storage
+            storageRef.putData(data, metadata: nil) { metadata, error in
+                guard let _ = metadata else {
+                    // Manejar el caso en el que ocurra un error al subir la imagen a Firebase Storage
+                    print("Error al subir la imagen a Firebase Storage:", error?.localizedDescription ?? "")
+                    return
+                }
+                
+                if let error = error {
+                    // Manejar el caso en el que ocurra un error al subir la imagen
+                    print("Error al subir la imagen:", error.localizedDescription)
+                    return
+                }
+                
+                // Imagen del perfil guardada con éxito en Firebase Storage
+            }
+        }
+    }
+
+
+    
+    
     
     func getNameUser(completion: @escaping (String?) -> Void) {
         let db = Firestore.firestore()
@@ -53,9 +106,35 @@ class ProfileViewModel:ObservableObject {
     }
 
     
+    func getProfileImage(completion: @escaping (UIImage?) -> Void) {
+            guard let currentUserID = Auth.auth().currentUser?.uid else {
+                // Manejar el caso en que el usuario actual no esté autenticado
+                completion(nil)
+                return
+            }
+
+            let storageRef = Storage.storage().reference().child("profile_images")
+            let userImageRef = storageRef.child("\(currentUserID).jpg")
+
+            userImageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+                if let error = error {
+                    // Manejar el caso en que ocurra un error al obtener la imagen
+                    print("Error al obtener la imagen del perfil:", error.localizedDescription)
+                    completion(nil)
+                } else {
+                    if let imageData = data, let profileImage = UIImage(data: imageData) {
+                        // Devolver la imagen del perfil
+                        completion(profileImage)
+                    } else {
+                        // No se pudo convertir los datos en una imagen
+                        completion(nil)
+                    }
+                }
+            }
+        }
+    
     
     
     }
 
-    
     
--- RestaurantsApp/RestaurantsApp/views/SideProfileView.swift
+++ RestaurantsApp/RestaurantsApp/views/SideProfileView.swift
@@ -3,7 +3,7 @@
 //  RestaurantsApp
 //
 //  Created by Villamil Pachon Leonidas on 18/03/24.
-//
+// edited by Luis Felipe Dussán
 
 import SwiftUI
 
@@ -11,8 +11,12 @@ struct SideProfileView: View {
     @StateObject var loginViewModel = LoginViewModel()
     @ObservedObject var profileViewModel = ProfileViewModel()
     @Binding var isShowing: Bool
-    var profileImage: Image // Imagen del perfil
-    
+    @State private var showingPreferences = false
+    @State private var selectedImage: UIImage? // Estado para almacenar la imagen seleccionada
+    @State private var isShowingImagePicker = false // Estado para controlar la presentación del selector de imágenes
+    @State private var profileImage: Image?
+    @State private var isLoadingImage = true
+       
     var body: some View {
         
         ZStack {
@@ -30,12 +34,37 @@ struct SideProfileView: View {
                     VStack(alignment: .center, spacing: 16) {
                         
                         // Imagen del perfil
-                        profileImage
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 230, height: 200)
-                            .clipShape(Circle())
-                            .padding(.bottom, 20)
+                        if isLoadingImage {
+                            // Si se está cargando la imagen, muestra un indicador de carga
+                            ProgressView("Cargando imagen...")
+                                .onAppear {
+                                    // Cuando la vista aparece, solicita la imagen del perfil
+                                    profileViewModel.getProfileImage { loadedImage in
+                                        // Una vez que la imagen se ha cargado, actualiza isLoadingImage y asigna la imagen al profileImage
+                                        isLoadingImage = false
+                                        if let loadedImage = loadedImage {
+                                            profileImage = Image(uiImage: loadedImage) // Convierte la UIImage a Image
+                                        }
+                                    }
+                                }
+                        } else {
+                            // Si la imagen se ha cargado, la muestra
+                            
+                                profileImage?
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 230, height: 200)
+                                    .clipShape(Circle())
+                                    .padding(.bottom, 20)
+                                    .onTapGesture {
+                                        // Acción al tocar la imagen
+                                    }
+                            
+                        }
+
+
+                                
+                        
                         
                         VStack(alignment: .leading, spacing: 10) {
                             
@@ -52,15 +81,12 @@ struct SideProfileView: View {
                                         .padding(.vertical, 10)
                                 )
                                 .onAppear {
-                                    // Llama a getNameUser() cuando la vista aparece para obtener el nombre del usuario
                                     profileViewModel.getNameUser { name in
                                         if let name = name {
-                                            // Actualiza profileName cuando se obtiene el nombre del usuario
                                             profileViewModel.profileName = name
                                         }
                                     }
                                 }
-                            
                             // Correo electrónico del perfil obtenido desde loginViewModel
                             if let savedCredentials = loginViewModel.getSavedCredentials() {
                                 let email = savedCredentials.email
@@ -69,19 +95,72 @@ struct SideProfileView: View {
                                     .frame(width: 260, height: 50)
                                     .overlay(
                                         Text("\(email)")
-                                            .font(.title)
+                                            .font(.subheadline) // Aquí cambia a .subheadline
                                             .fontWeight(.bold)
                                             .foregroundColor(.black)
                                             .padding(.horizontal, 20)
                                             .padding(.vertical, 10)
                                     )
+                                
                             } else {
                                 Text("No se ha guardado ningún correo electrónico")
-                                    .foregroundColor(.red) // Opcional: Si deseas un color específico para este caso
+                                    .foregroundColor(.red)
                             }
                             
-                            // Poner acá el restaurante del que más pide ordenes
+                            // Botón para las preferencias
+                            Button(action: {
+                                showingPreferences.toggle()
+                            }) {
+                                Text("Preferencias")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .padding(.horizontal, 25)
+                                    .padding(.vertical, 10)
+                                    .background(Constants.Alerts)
+                                    .cornerRadius(8)
+                            }.sheet(isPresented: $showingPreferences) {
+                                PreferencesView()
+                            }
                             
+                            Button(action: {
+                                isShowingImagePicker = true
+                            }) {
+                                Text("Cambiar Imagen")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .padding(.horizontal, 25)
+                                    .padding(.vertical, 10)
+                                    .background(Constants.Alerts)
+                                    .cornerRadius(8)
+                            }
+                            
+                            if selectedImage != nil{
+                                
+                                Text("Se cargó la imagen, guarda los cambios")
+                                    .padding()
+                                    .foregroundColor(.white)
+                                
+                                // Botón para guardar los datos y cerrar la vista
+                                Button(action: {
+                                    if let image = selectedImage {
+                                        profileViewModel.saveProfileImage(image: image)
+                                        profileImage = Image(uiImage: image)
+                                        selectedImage = nil // ¿Podría ser este el origen del error?
+                                    }
+                                }) {
+                                    Text("Guardar")
+                                        .fontWeight(.medium)
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Constants.pinkColor)
+                                        .cornerRadius(8)
+                                }
+
+
+                                
+                            }
                         }
                         
                         // Más detalles del perfil, si es necesario
@@ -91,6 +170,11 @@ struct SideProfileView: View {
                     .padding()
                     .frame(width: 270, alignment: .leading)
                     .background(Color(red: 0.51, green: 0.77, blue: 0.75))
+                    .cornerRadius(20)
+                    .shadow(radius: 5)
+                    .sheet(isPresented: $isShowingImagePicker) {
+                        ImagePicker(selectedImage: $selectedImage, isPresented: $isShowingImagePicker)
+                    }
                 }
                 
                 ScrollView {
@@ -101,4 +185,105 @@ struct SideProfileView: View {
             }
         }
     }
+    
+    struct Constants {
+        static let Alerts: Color = Color(red: 0.78, green: 0.59, blue: 0.75)
+        static let pinkColor = Color.pink // Agrega este color para usarlo en la vista
+    }
 }
+
+
+
+struct PreferencesView: View {
+    @State private var selectedOptions: Set<String> = []
+
+    var body: some View {
+        VStack {
+            Text("Selecciona tus preferencias")
+                .font(.headline)
+                .padding()
+
+            HStack {
+                Button(action: {
+                    if selectedOptions.contains("Vegetariana") {
+                        selectedOptions.remove("Vegetariana")
+                    } else {
+                        selectedOptions.insert("Vegetariana")
+                    }
+                }) {
+                    Text("Vegetariana")
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(selectedOptions.contains("Vegetariana") ? .white : .black)
+                }
+                .background(selectedOptions.contains("Vegetariana") ? Color.green : Color.gray)
+                .cornerRadius(8)
+
+                Button(action: {
+                    if selectedOptions.contains("Comida de mar") {
+                        selectedOptions.remove("Comida de mar")
+                    } else {
+                        selectedOptions.insert("Comida de mar")
+                    }
+                }) {
+                    Text("Comida de mar")
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(selectedOptions.contains("Comida de mar") ? .white : .black)
+                }
+                .background(selectedOptions.contains("Comida de mar") ? Color.green : Color.gray)
+                .cornerRadius(8)
+            }
+
+            HStack {
+                Button(action: {
+                    if selectedOptions.contains("Pasta") {
+                        selectedOptions.remove("Pasta")
+                    } else {
+                        selectedOptions.insert("Pasta")
+                    }
+                }) {
+                    Text("Pasta")
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(selectedOptions.contains("Pasta") ? .white : .black)
+                }
+                .background(selectedOptions.contains("Pasta") ? Color.green : Color.gray)
+                .cornerRadius(8)
+
+                Button(action: {
+                    if selectedOptions.contains("Saludable") {
+                        selectedOptions.remove("Saludable")
+                    } else {
+                        selectedOptions.insert("Saludable")
+                    }
+                }) {
+                    Text("Saludable")
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(selectedOptions.contains("Saludable") ? .white : .black)
+                }
+                .background(selectedOptions.contains("Saludable") ? Color.green : Color.gray)
+                .cornerRadius(8)
+            }
+
+            Button(action: {
+                // Aquí puedes guardar las opciones seleccionadas
+            }) {
+                Text("Guardar")
+                    .fontWeight(.bold)
+                    .padding()
+                    .foregroundColor(.white)
+            }
+            .background(Color.blue)
+            .cornerRadius(8)
+        }
+        .padding()
+        .frame(maxHeight: .infinity) // Expandir verticalmente
+        .background(Color.white)
+        .cornerRadius(10)
+        .padding()
+    }
+}
+
+
